b80a942aed7b3a7b1de10bebac6ae050eb691b6f
Anshul1563 <pesanshul@gmail.com>
2023-02-11 23:47:24 +0530

    Added analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
+import { AnalyticsWrapper } from "../components/Analytics";
 
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head />
 			<body>{children}</body>
+			<AnalyticsWrapper />
 		</html>
 	);
 }
