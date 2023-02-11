43b0018e5aa1107408b8fe91807a04e9f761e50e
Anshul1563 <pesanshul@gmail.com>
2023-02-11 23:54:15 +0530

    analytics fix

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,8 +5,10 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head />
-			<body>{children}</body>
-			<AnalyticsWrapper />
+			<body>
+				{children}
+				<AnalyticsWrapper />
+			</body>
 		</html>
 	);
 }
